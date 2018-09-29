ad2b3f61206a9aefbf8df047dc4c4b41895ce384
addai.5 <addai.5@buckeyemail.osu.edu>
2018-09-29 18:15:39 +0000

    Done.

--- say_hello.rb
+++ say_hello.rb
@@ -1,1 +1,8 @@
 # Build your say_hello method here
+def say_hello(language = "Ruby Programmer!", name)
+  if name != nil 
+    puts "Hello " + name + "!"
+  else
+    puts "Hello " + language
+end
+
